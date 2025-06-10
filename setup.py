5974efbc05dd305ada2297a5f154127049edfb95
Macro Evolution <macrolabclemson@gmail.com>
2025-06-10 11:59:27 -0400

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'segmentation',
+    version = '0.0.0',
+    author = 'Syed Shahzad Abdul Wajid',
+    author_email = 'shahzadksa777@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+)
